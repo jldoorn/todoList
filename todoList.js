852b3dfe86715f3e62e64c001da016530d1033c3
Jonathan Doorn <jdoorn@purdue.edu>
2021-03-31 11:14:18 -0400

    List entries working with vue component

--- todoList.js
+++ todoList.js
@@ -8,6 +8,42 @@ const firebaseConfig = {
     appId: "1:667879597898:web:64a90b4d399bdf223803b3"
 };
 // Initialize Firebase
+//
+// firebase.initializeApp(firebaseConfig);
+// let database = firebase.database();
 
-firebase.initializeApp(firebaseConfig);
-let database = firebase.database();
+Vue.component('list-entry', {
+    props: ['time', 'description'],
+    template: `
+      <li class="list-group-item">
+      <div class="container">
+      <div class="row">
+      <div class="col-2">{{ time }}</div>
+      <div class="col">{{ description }}</div>
+      <div class="col-2"><button type="button" class="btn btn-outline-secondary"><i class="bi-check bi-check2"></i></button></div></div>
+      </div>
+      </li>
+    `
+});
+
+Vue.component('prop-component', {
+    props: ['text', 'anotherprop'],
+    template: `
+      <div>
+      <h3>This is a prop-based component</h3>
+      <p>It has some props, but no data and no functionality</p>
+      <p>The {{text}} jumped over the {{anotherprop}}.</p>
+      </div>
+    `
+});
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        todo: [
+            {time: "May 1, 11:25A", description: "Do laundry"},
+            {time: "May 2, 11:25A", description: "Do shopping"},
+            {time: "May 3, 11:25A", description: "Do chores"},
+        ]
+    }
+})
